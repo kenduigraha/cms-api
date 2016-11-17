f01d7d70a0dd3263a60af4e8a732d1272be3d7a8
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-17 14:36:15 +0700

    add login & register script

---
+++ client/script/login.js
@@ -0,0 +1,42 @@
+const Auth = {
+  getToken: () => {
+    return localStorage.getItem('token')
+  },
+  getUser: () => {
+    let token = Auth.getToken()
+    if (!token) return {}
+    else {
+      return jwt_decode(token)
+    }
+  }
+}
+
+$(document).ready(function(){
+  $('#btn_login').on('click', function(e){
+    e.preventDefault()
+    processLogin()
+  })
+})
+
+function processLogin(){
+  var data_login = {
+    username: $('#username').val(),
+    password: $('#password').val()
+  }
+
+  $.post({
+    url: "http://localhost:3000/api/users/login",
+    data: data_login,
+    success: function(login_user){
+      localStorage.setItem('token', login_user.token)
+
+      if(Auth.getUser().username){
+        window.location = 'home.html'
+      }else{
+        alert('input is wrong')
+        localStorage.removeItem('token')
+        window.location = 'login.html'
+      }
+    }
+  })
+}
---
+++ client/script/register.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  //process register
+  $('#btn_register').on('click', function(e){
+   e.preventDefault()
+   processRegister()
+  })
+})
+
+function processRegister(){
+  var data = {
+    username: $('#username').val(),
+    password: $('#password').val(),
+    email: $('#email').val()
+  }
+
+  $.post({
+    url: 'http://localhost:3000/api/users',
+    data: data,
+    success: function(new_user){
+      // console.log(new_user);
+      localStorage.setItem('token', new_user.token)
+      window.location = 'home.html'
+      
+      $('#username').val('')
+      $('#password').val('')
+      $('#email').val('')
+    }
+  })
+}
