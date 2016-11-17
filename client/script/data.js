74831373b36c3b2d972e7bb5fc3a36e4e497cf1e
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-17 15:24:31 +0700

    CRUD data

--- client/script/data.js
+++ client/script/data.js
@@ -33,8 +33,124 @@ $(document).ready(function(){
     $('#show_form_add').show()
   })
 
+  //show all data
   showAllData()
+
+  //process new data
+  $('#btn_add').on('click', function(e){
+    e.preventDefault()
+    processNewData()
+  })
+
+  //process updated data
+  $('#btn_update').on('click', function(e){
+    e.preventDefault()
+    processUpdate()
+  })
 })
+
+function processUpdate(){
+  var new_edited = {
+    letter: $('#letter').val(),
+    frequency: $('#frequency').val()
+  }
+  var id = $('#id').val()
+  $.ajax({
+    url: 'http://localhost:3000/api/datas/'+id,
+    method: 'PUT',
+    data: new_edited,
+    success: function(new_data){
+      console.log(new_data);
+      var data_HTML = `
+        <tr id="${new_data._id}">
+          <td>
+            ${new_data.letter}
+          </td>
+          <td>
+            ${new_data.frequency}
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning" onclick="submitEditButton('${new_data._id}')">Edit</button>
+            <button type="button" class="btn btn-danger" onclick="submitDeleteButton('${new_data._id}')">Delete</button>
+          </td>
+        </tr>
+      `
+      $(`#${new_data._id}`).replaceWith(data_HTML)
+
+      $('#letter').val('')
+      $('#frequency').val('')
+
+      hideAddForm()
+      $('#show_form_add').show()
+    }
+  })
+}
+
+function submitEditButton(id){
+  $.ajax({
+    url: 'http://localhost:3000/api/datas/'+id,
+    method: 'PUT',
+    success: function(updated_data){
+      console.log(updated_data);
+      showAddForm()
+      $('#letter').val(updated_data.letter)
+      $('#frequency').val(updated_data.frequency)
+      $('#btn_update').show()
+      $('#btn_add').hide()
+      $('#id').val(updated_data._id)
+    }
+  })
+}
+
+function submitDeleteButton(id){
+  var r = confirm("Are you sure want to delete?");
+  if (r == true) {
+    $.ajax({
+      url: 'http://localhost:3000/api/datas/'+id,
+      method: "DELETE",
+      success: function(deleted_data){
+        $(`#${deleted_data._id}`).remove()
+      }
+    })
+  } else {
+
+  }
+}
+
+function processNewData(){
+  var new_data = {
+    letter: $('#letter').val(),
+    frequency: $('#frequency').val()
+  }
+  $.post({
+    url: 'http://localhost:3000/api/datas',
+    data: new_data,
+    success: function(new_data){
+      console.log(new_data);
+      var data_HTML = `
+        <tr id="${new_data._id}">
+          <td>
+            ${new_data.letter}
+          </td>
+          <td>
+            ${new_data.frequency}
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning" onclick="submitEditButton('${new_data._id}')">Edit</button>
+            <button type="button" class="btn btn-danger" onclick="submitDeleteButton('${new_data._id}')">Delete</button>
+          </td>
+        </tr>
+      `
+      $('#body_table').prepend(data_HTML)
+
+      $('#letter').val('')
+      $('#frequency').val('')
+
+      hideAddForm()
+      $('#show_form_add').show()
+    }
+  })
+}
 
 function showAllData(){
   $.ajax({
@@ -44,9 +160,18 @@ function showAllData(){
       var data_HTML = ``
       for (var i = 0; i < all_data.length; i++) {
         data_HTML += `
-        <div id="${all_data[i]._id}">
-
-        </div>
+        <tr id="${all_data[i]._id}">
+          <td>
+            ${all_data[i].letter}
+          </td>
+          <td>
+            ${all_data[i].frequency}
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning" onclick="submitEditButton('${all_data[i]._id}')">Edit</button>
+            <button type="button" class="btn btn-danger" onclick="submitDeleteButton('${all_data[i]._id}')">Delete</button>
+          </td>
+        </tr>
         `
       }
 
@@ -58,8 +183,8 @@ function showAllData(){
 function showAddForm(){
   $('#show_form_hide').show()
   $('#form_new_data').show()
-  $('#btn_update').show()
-  $('#btn_add').hide()
+  $('#btn_update').hide()
+  $('#btn_add').show()
   $('#show_form_add').hide()
 }
 
